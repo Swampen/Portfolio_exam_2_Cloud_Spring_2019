789476917829a216fe131ff40436a39ccb7b2b88
MichaelThoresen <mimothor@hotmail.com>
2019-04-23 22:04:45 +0200

    removed Scripts related to connecting to ALTO

--- openstack-connect-1-0-1.sh
+++
@@ -1,8 +0,0 @@
-# /bin/bash
-export OS_USERNAME="dats06"
-echo "Please enter your OpenStack Password: "
-read -sr OS_PASSWORD_INPUT
-export OS_PASSWORD="$OS_PASSWORD_INPUT"
-export OS_PROJECT_NAME="dats06_project"
-export OS_IDENTITY_API_VERSION=3
-export OS_AUTH_URL=https://cloud.cs.hioa.no:5000/v3
