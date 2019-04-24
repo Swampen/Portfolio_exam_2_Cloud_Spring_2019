13525beb200bb2be30c2e5ce916a49be6157da7c
Swampen <ole-ma@outlook.com>
2019-04-24 16:48:13 +0200

    small updates to hostfile_setup

--- scripts/hostfile_setup.sh
+++ scripts/hostfile_setup.sh
@@ -6,18 +6,19 @@ ipSubnet="10\.10"
 for vm in ${!vmnames[@]}; do
         name=${vmnames[$vm]}
         echo "Getting IP for $name"
-        ip=$(openstack server show $name | grep -o "$subnet\.[0-9]*\.[0-9]*")
+        ip=$(openstack server show $name | grep -o "$ipSubnet\.[0-9]*\.[0-9]*")
+	echo "$ip"
         ipList="$ipList $ip"
         ipEntry="$ip $name \\n$ipEntry"
 done
 
 username="ubuntu"
-sshkey="~.ssh/dats06-key.pem"
+sshkey="~/.ssh/dats06-key.pem"
 GW_USER="dats06"
 GW_HOST="dats.vlab.cs.hioa.no"
-sshProxyCommand="ssh -q $GW_USER@$GW_HOST -W %h:%p"
+sshProxyCommand="ssh  $GW_USER@$GW_HOST -W %h:%p"
 
-parallel-ssh --askpass -i -H "$ipList" \
+parallel-ssh -i -H "$ipList" \
         -l $username \
-        -x "-i ~/.ssh/dats06-key.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ProxyCommand=$sshProxyCommand" \
+        -x "-i $sshkey -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ProxyCommand='$sshProxyComand'" \
         -t 1800 "sudo sed -i '1 i\\$ipEntry' /etc/hosts;"
