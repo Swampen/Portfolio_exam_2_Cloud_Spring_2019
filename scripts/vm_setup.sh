dd4b70116d2e0cbcf59bedc32a32c35bc5990607
MichaelThoresen <32193552+MichaelThoresen@users.noreply.github.com>
2019-04-26 20:08:28 +0200

    Added comments to the vm_setup.sh script

--- scripts/vm_setup.sh
+++ scripts/vm_setup.sh
@@ -5,13 +5,15 @@ DBPOK=false
 DBOK=false
 # Creates the security group
 openstack security group create dats06-security-1
+#sleeps for a little while so that the script will register that the SecGroup has been made
 sleep 4
 #Adds appropriate rules to the security group
-#Permitting ssh from the outside
+#Permitting ssh internally
 openstack security group rule create \
 	--protocol tcp \
 	--dst-port 22 \
 	--remote-group dats06-security-1 dats06-security-1
+#Permits ssh from the outside
 openstack security group rule create \
        	--protocol tcp \
        	--remote-ip 0.0.0.0/0 \
@@ -38,7 +40,8 @@ openstack security group rule create \
 	--protocol tcp \
 	--dst-port 4568 \
 	--remote-group dats06-security-1 dats06-security-1
-#Creates key-pair
+
+#Checks if the .ssh directory exists and if it doesn't make it and gives it permissions 755
 if [ -d "$HOME/.ssh/" ]
 then
 	echo "/.ssh exists"
@@ -47,12 +50,16 @@ else
 	chmod 755 ~/.ssh
 fi
 
+#Creates keypair and puts it in the .ssh folder and gives it permission 400 so that no one except the owner can read it
 openstack keypair create dats06-1-key > ~/.ssh/dats06-1-key.pem
 chmod 400 ~/.ssh/dats06-1-key.pem
+#Sleep to let the system get some time to register that the key has been made
 sleep 4
 
+#While loop which checks if the web servers, databases and database proxy are up and running correctly
 while [[ $WebOK = false ]] || [[ $DBPOK = false ]] || [[ $DBOK = false ]]
 do
+	#If the web servers aren't running make them
 	if [[ $WebOK = false ]]
 	then
 		#Create VM going here
@@ -66,6 +73,7 @@ do
        			--min 1 --max 3 --wait dats06-web1
 	fi
 
+	#If the database proxy isn't up and running make it
 	if [[ $DBPOK = false ]]
 	then
 		#Create VM going here
@@ -79,6 +87,7 @@ do
 			--min 1 --max 1 --wait dats06-dbproxy1
 	fi
 
+	#If the databases aren't up and running make them
 	if [[ $DBOK = false ]]
 	then
 		#Create VM going here
@@ -92,9 +101,10 @@ do
 			--min 1 --max 3 --wait dats06-db1
 	fi
 
-
+	#Puts the name of all the servers which failed to start in an array
 	failed=`openstack server list --status ERROR -c Name | awk '!/^$|Name/ {print $2;}'`
 
+	#Checks if the array is empty, and if it does set all booleans to true
 	if [[ ${#failed} = 0 ]]
 	then
 		WebOK=true
@@ -102,6 +112,7 @@ do
 		DBPOK=true
 	fi
 
+	#Goes through the list and deletes the VMs that are not running correctly so that they can be relaunched, if not then set the corresponding boolean to true
 	for i in $failed
 	do
 		if [[ $i =~ (dats06-db1-)([1-9]) ]] 
