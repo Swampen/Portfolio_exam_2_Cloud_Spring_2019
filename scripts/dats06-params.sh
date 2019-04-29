f85952b7864dfab5dae7fb5bebd98511c341756a
Swampen <ole-ma@outlook.com>
2019-04-29 14:57:32 +0200

    Added actual global variables

--- scripts/dats06-params.sh
+++ scripts/dats06-params.sh
@@ -1,38 +1,38 @@
 ################# Instance Naming Parameters ###############
 # Security group to use
-SECURITYGROUP="dats06-security"
+export securityGroup="dats06-security"
 
 # Names in cloud setup
-DBNAME="dats06-db"
-NUMBEROFDBS=3
-DBPROXYNAME="dats06-dbproxy"
-WEBSERVERNAME="dats06-web"
-NUMBEROFWEBSERVERS=3
+export DBName="dats06-db"
+export numberOfDBs=3
+export DBProxyName="dats06-dbproxy"
+export webServerName="dats06-web"
+export numberOfWebServers=3
 
 # Naming of hosts
-DBHOSTNAME="db"
-DBPROXYNAME"maxscale"
-WEBSERVERHOSTNAME="web"
-LOADBALANCERHOSTNAME="lb"
+export DBHostName="db"
+export DBProxyHostName="maxscale"
+export webServerHostName="web"
+export loadBalancerHostName="lb"
 
 
 
 ################# VM setup paremeters #################
 # Locale parameter
-USERLOCALE="nb_NO.UTF-8"
+export userLocale="nb_NO.UTF-8"
 
 ################# dbSetup Parameters ######################
 
 
 
 # This is the name to be used for creation of the keypair used for authentication
-KEYPAIRNAME="dats06-key"
+export keyPairName="dats06-key"
 
 # This is the loctation for the private-key to be used for accessing the vm
-KEYLOCATION="~/.ssh/$KEYPAIRNAME.pem"
+export keyLocation="~/.ssh/$KEYPAIRNAME.pem"
 
 # Usename for logging into vms
-USER="ubuntu"
+export user="ubuntu"
 
 # Proxycommand for ssh into vms
-PROXYCOMMAND="ssh dats06@dats.vlab.cs.hioa.no -W %h:%p"
+export proxyCommand="ssh dats06@dats.vlab.cs.hioa.no -W %h:%p"
