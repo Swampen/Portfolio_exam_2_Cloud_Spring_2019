9614bfa0a8f22448205008d57d953ea1cdda0e80
Swampen <ole-ma@outlook.com>
2019-05-02 20:40:32 +0200

    Beginning to implement hostname change

--- scripts/hostfile_setup.sh
+++ scripts/hostfile_setup.sh
@@ -6,15 +6,19 @@ echo "##### Editing /etc/hosts files #####"
 #vmnames=(`openstack server list -c Name | awk '!/^$|Name/ {print $2;}'`)
 
 ipSubnet="10\.10"
-for vm in ${!vmnames[@]}; do
-        name=${vmnames[$vm]}
-        echo -n "Getting IP for $name: "
-        ip=$(openstack server show $name | grep -o "$ipSubnet\.[0-9]\{1,3\}\.[0-9]\{1,3\}")
-	echo "$ip"
-        ipList="$ipList $ip"
-        ipEntry="$ip $name \\n$ipEntry"
-done
-
+#names=()
+#ipList=()
+#for vm in ${vmnames[@]}; do
+#        name=$vm
+#        echo -n "Getting IP for $name: "
+#        ip=$(openstack server show $name | grep -o "$ipSubnet\.[0-9]\{1,3\}\.[0-9]\{1,3\}")
+#	echo "$ip"
+#        ipList+=("$ip")
+#        ipEntry="$ip $name \\n$ipEntry"
+        #names+=("$name")
+#done
+echo rip
+sleep 2
 username="ubuntu"
 sshkey="~/.ssh/dats06-key.pem"
 MASTER_USER="dats06"
@@ -23,18 +27,14 @@ sshProxyCommand="ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/n
 
 # sed s/dats06-db-/db/g
 # sed s/dats06-web-/web/g
-# sed s/dats06-dbproxy/maxscale/g
-
-# FOR TESTING #
-ipList="10.10.6.147"
-ipEntry="10.10.6.147 vmtest"
+# sed s/dats06-dbproxy/maxscale/
 
 script=("
 sudo sed -i '1 i\\$ipEntry' /etc/hosts;
 sudo sed -i '$ a LANGUAGE=\"$USERLOCALE\"\nLC_ALL=\"$USERLOCALE\"' /etc/default/locale;
 sudo unlink /etc/localtime;
 sudo ln -s /usr/share/zoneinfo/Europe/Oslo /etc/localtime;
-sudo locale-gen $USERLOACALE;
+sudo locale-gen $userLocale;
 sudo sed -i s/XKBLAYOUT=.*/XKBLAYOUT=\"no\"/g /etc/default/keyboard;
 ")
 
@@ -43,12 +43,35 @@ echo Hello World;
 hostname;
 ")
 
-parallel-ssh -i -H "$ipList" \
+parallel-ssh -i -H "${ipList[*]}" \
         -l $username \
         -x "-i ~/.ssh/dats06-key.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR -o ProxyCommand='$sshProxyCommand'" \
-        "$script"
+        "$test"
 
-echo "Rebooting the VMs"
-for vm in ${!vmnames[@]}; do
-	openstack server reboot --wait ${!vmnames[$vm]}
+echo "Changing hostname and rebooting the VMs"
+
+command='sudo bash -c "echo"'
+for i in ${!vmnames[@]}; do
+	let n=i+1
+	echo -n "$n/${#vmnames[@]} - ${vmnames[$i]} - "
+
+	check=`echo "${vmnames[$i]}" | grep -o "$DBName-\?[0-9]*"`
+	ip=""
+	hostname=""
+	if [[ $check = $DBName-[0-9]* ]]; then
+		hostname=`echo ${vmnames[$i]} | sed s/dats06-db-/db/g`
+		echo $hostname
+		ip=${ipList[$i]}
+		ssh -i ~/.ssh/dats06-key.pem $username@$ip -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR -o ProxyCommand="$sshProxyCommand" "sudo bash -c 'echo $hostname > /etc/hostname'"
+	fi
+
+	check=`echo "${vmnames[$i]}" | grep -o "$DBProxyName"`
+	if [[ $check = $DBProxyName ]]; then
+		hostname=`echo ${vmnames[$i]} | sed s/dats06-dbproxy/maxscale/g`
+		echo $hostname
+		ip=${ipList[$i]}
+		ssh -i ~/.ssh/dats06-key.pem $username@$ip -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR -o ProxyCommand="$sshProxyCommand" "sudo bash -c 'echo $hostname > /etc/hostname'"
+	fi
+	openstack server reboot --wait ${vmnames[$i]}
 done
+# ssh -i ~/.ssh/dats06-key.pem ubuntu@10.10.4.41 -o ProxyCommand="ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR $MASTER_USER@$MASTER_HOST -W %h:%p"
