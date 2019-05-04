125d29cd5a0f99d22a2290661a242fb11cf4d192
Swampen <ole-ma@outlook.com>
2019-05-05 00:49:36 +0200

    Working stats page for haproxy monitoring

--- scripts/lb_setup.sh
+++ scripts/lb_setup.sh
@@ -11,15 +11,14 @@ config"frontend web-frontend
 	default_backend web-backend
 
 backend web-backend
-	balance roundrobin
-	mode http
-	option httpchk HEAD / HTTP/1.1\r\nHost:\ localhost
-	server web-1 web1:80 check weight 10
-	server web-2 web2:80 check weight 10
-	server web-3 web3:80 check weight 10"
-#listen haproxy-monitoring
-#       stats enable
-#       stats refresh 30s
-#       stats uri /stats
-#       stats realm Haproxy\ Statistics
-#       stats auth dats06:"thrown similar river"
+    balance roundrobin
+    mode http
+    option httpchk HEAD / HTTP/1.1\r\nHost:\ localhost
+    server web-1 web1:80 check weight 10
+    server web-2 web2:80 check weight 10
+    server web-3 web3:80 check weight 10
+    stats enable
+    stats refresh 30s
+    stats uri /stats
+    stats realm Haproxy\ Statistics
+    stats auth dats06:\"thrown similar river\""
