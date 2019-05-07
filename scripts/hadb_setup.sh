84d5fd35463f57074c1bdc1c593086af3e72c65f
fredrikfrostad <frostad.fredrik@gmail.com>
2019-05-07 13:39:14 +0200

    Har fikset oppretting av configfil på dbservere

--- scripts/hadb_setup.sh
+++ scripts/hadb_setup.sh
@@ -15,20 +15,20 @@ echo "Getting dbServer IPs"
 for ((i = 1; i <= $numberOfDBs; i++))
 do
 	#Getting IP-address of instance
-   	hostIP=$(openstack server show $DBName-$i | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
-	DBHOSTS="$DBHOSTS $hostIP"
+   	#hostIP=$(openstack server show $DBName-$i | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
+	hostName="$DBHostName$i"
+	DBHOSTS="$DBHOSTS$hostName "
 done
 
-
 # Extracting MaxScale host IP from hostname
-SCALEHOST=$(openstack server show $DBPROXYNAME | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
+#SCALEHOST=$(openstack server show $DBProxyName | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
 
 # Generating host configstring for galeraserver
-galerahoststring=$( echo wsrep_cluster_address=gcomm://${DBHOSTNAMES[*]} | tr " " ",")
-
+galerahoststring=$( echo wsrep_cluster_address=gcomm://${DBHOSTS[*]} | tr " " ",")
 
 
 ############### SETUP COMMANDS TO BE RUN ON DB SERVERS###############
+
 dbSetupCommands=("
 sudo locale-gen "nb_NO.UTF-8";
 sudo apt-get update -y;
@@ -39,15 +39,49 @@ sudo apt-get update -y;
 sudo DEBIAN_FRONTEND=noninteractive apt-get install mariadb-server rsync -y;
 ")
 
+for ((i = 1; i <= $numberOfDBs; i++))
+do
+dbConfigString=("
+[mysqld]
+binlog_format=ROW
+default-storage-engine=innodb
+innodb_autoinc_lock_mode=2
+bind-address=0.0.0.0
+
+# Galera Provider Configuration
+nwsrep_on=ON
+wsrep_provider=/usr/lib/galera/libgalera_smm.so
+
+# Galera Cluster Configuration
+nwsrep_cluster_name="galera_cluster"
+wsrep_cluster_address="$galerahoststring"
+
+# Galera Synchronization Configuration
+wsrep_sst_method=rsync
+
+# Galera Node Configuration
+wsrep_node_address="$DBHostName$i"
+wsrep_node_name="$DBHostName$i"
+")
+
+command=("
+sudo echo '$dbConfigString' > ~/tmpfile
+sudo cp /home/ubuntu/tmpfile /etc/mysql/conf.d/galera.cnf
+")
+echo $dbConfigString
+#parallel-ssh -i -H "$DBHostName$i" -l "$username" -x "-i $sshKeyLocation -o StrictHostKeyChecking=no -o ProxyCommand='$sshProxyCommand'" "$command"
+ssh -i "$sshKeyLocation" -o ProxyCommand="$sshProxyCommand" "$username@$DBHostName$i" "$command"
+exit 0
+done
+
 #export DEBIAN_FRONTEND=noninteractive;
 #sudo debconf-set-selections <<< 'mariadb-server-10.1 mysql-server/root_password password PASS';
 #sudo debconf-set-selections <<< 'mariadb-server-10.1 mysql-server/root_password_again password PASS';
-############## SETUP COMMANDS TO BE RUN ON MAXSCALE SERVER #################
 
 
 echo "Starting dbServer setup"
-#parallell-ssh --askpass -i -H "$DBHOSTS" -l "$USERNAME" -x "-i $sshkey -o StrictHostKeyChecking=no "  < mkdir TEST
-parallel-ssh -i -H "$DBHOSTS" -l "$username" -x "-i $keyLocation -o StrictHostKeyChecking=no -o ProxyCommand='$sshProxyCommand'" "$dbSetupCommands"
+#parallell-ssh --askpass -i -H "$DBHOSTS" -l "$username" -x "-i $sshkey -o StrictHostKeyChecking=no "  < mkdir TEST
+parallel-ssh -i -H "$DBHOSTS" -l "$username" -x "-i $keyLocation -o StrictHostKeyChecking=no -o ProxyCommand='$sshProxyCommand'" "dbSetupCommands"
 
 echo "Done running parallel ssh commands"
 
@@ -55,3 +89,7 @@ echo "Done running parallel ssh commands"
 
 # Command for getting ip of instance if we know the name
 #nova list --name dbtest-1 | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'
+
+################### SETUP COMMANDS TO BE RUN ON MAXSCALE SERVERS #####################
+
+
