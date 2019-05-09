add43196c496d43a9c68833be84299679ee7b879
MichaelThoresen <mimothor@hotmail.com>
2019-05-09 20:54:55 +0200

    Fixed typo in comments

--- scripts/vm_setup.sh
+++ scripts/vm_setup.sh
@@ -141,7 +141,7 @@ do
 	for i in $failed
 	do
 
-		# If i corresponds with the web server name with a - and a number at the end
+		# If it corresponds with the web server name with a - and a number at the end
 		# delete said server, if i does not match then set corresponding boolean to true
 		if [[ $i =~ ($webServerName-)([1-9]) ]]
 		then
@@ -151,7 +151,7 @@ do
 			WebOK=true
 		fi
 
-		# If i corresponds with the database proxy name
+		# If it corresponds with the database proxy name
 		# delete said server, if i does not match then set corresponding boolean to true
 		if [[ $i = "$DBProxyName" ]]
 		then
@@ -161,7 +161,7 @@ do
 			DBPOK=true
 		fi
 
-		# If i corresponds with the database name with a - and a number at the end
+		# If it corresponds with the database name with a - and a number at the end
 		# delete said server, if i does not match then set corresponding boolean to true
 		if [[ $i =~ ($DBName-)([1-9]) ]]
 		then
