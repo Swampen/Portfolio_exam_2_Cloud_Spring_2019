93ed6de126cc7890d6de39c5f6f0df0106eec096
fredrikfrostad <frostad.fredrik@gmail.com>
2019-05-10 14:08:41 +0200

    Added missing files

---
+++ scripts/InstanceRebuildUbuntu16_04.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# This script rebiulds all VMs to base ubuntu 16.04 image without deleting the VMs.
+
+# Sourcing openstack 
+source "$dir"/dats06_project-openrc.sh
+
+nova rebuild dats06-lb Ubuntu16.04
+nova rebuild dats06-web-1 Ubuntu16.04
+nova rebuild dats06-web-2 Ubuntu16.04
+nova rebuild dats06-web-3 Ubuntu16.04
+nova rebuild dats06-dbproxy Ubuntu16.04
+nova rebuild dats06-db-1 Ubuntu16.04
+nova rebuild dats06-db-2 Ubuntu16.04
+nova rebuild dats06-db-3 Ubuntu16.04
+
+echo "Rebuild done!!"
